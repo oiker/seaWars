b75c4148bab14dd513c05545f49cacdfb08782bf
Pchelintsev <pchelintsev@teamidea.ru>
2020-07-23 22:41:42 +0300

    Initial commit

---
+++ src/main/java/dushkof/seaWars/SeaWarsApplication.java
@@ -0,0 +1,13 @@
+package dushkof.seaWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dushkof/seaWars/SeaWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package dushkof.seaWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
