f418aaf1691484c7e7f995a3d56c8b9a25b41eda
Pchelintsev <pchelintsev@teamidea.ru>
2020-07-24 03:20:02 +0300

    add spring application context xml file

--- src/main/java/dushkof/seaWars/controllers/HelloController.java
+++ src/main/java/dushkof/seaWars/controllers/HelloController.java
@@ -1,14 +1,21 @@
 package dushkof.seaWars.controllers;
 
+import dushkof.seaWars.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("hello")
 public class HelloController {
+
+    @Resource(name = "userService")
+    private UserService userService;
+
     @GetMapping
     public String list() {
-        return "hyi";
+        return userService.sayHi();
     }
 }
---
+++ src/main/java/dushkof/seaWars/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package dushkof.seaWars.services.impl;
+
+import dushkof.seaWars.services.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public String sayHi() {
+        return "hi";
+    }
+}
