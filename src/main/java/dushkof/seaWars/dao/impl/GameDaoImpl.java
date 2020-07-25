17f528f2990992e610ae4ec1391394242b5f22c2
Pchelintsev <pchelintsev@teamidea.ru>
2020-07-25 14:57:26 +0300

    little refactor

--- src/main/java/dushkof/seaWars/dao/impl/GameDaoImpl.java
+++ src/main/java/dushkof/seaWars/dao/impl/GameDaoImpl.java
@@ -1,32 +1,36 @@
 package dushkof.seaWars.dao.impl;
 
 import dushkof.seaWars.dao.GameDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class GameDaoImpl implements GameDao {
-    private DriverManagerDataSource dataSource;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameDaoImpl.class);
+    private static final String CREATE_USER_TABLE = "CREATE TABLE users( ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, NAME CHAR(20) NOT NULL UNIQUE , PASSWORD CHAR(10) NOT NULL);";
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public String init() {
         try {
             // создаем таблицы и возвращаем ОК в случае успеха
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-            jdbcTemplate.execute("CREATE TABLE users( ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, NAME CHAR(20) NOT NULL UNIQUE , PASSWORD CHAR(10) NOT NULL);");
+            LOGGER.info("Start DB initialization");
+            getJdbcTemplate().execute(CREATE_USER_TABLE);
+            LOGGER.info("DB initialization is success");
             return "OK";
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return e.getMessage();
+            LOGGER.error(e.getMessage(), e);
+            return "NOK";
         }
     }
 
-    @Required
-    public void setDataSource(DriverManagerDataSource dataSource) {
-        this.dataSource = dataSource;
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
     }
 
-    public DriverManagerDataSource getDataSource() {
-        return dataSource;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
