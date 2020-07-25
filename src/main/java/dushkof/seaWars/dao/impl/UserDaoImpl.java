be4f7d5554de2d178702d9ca5cec33d164509449
Pchelintsev <pchelintsev@teamidea.ru>
2020-07-25 14:57:26 +0300

    little refactor

--- src/main/java/dushkof/seaWars/dao/impl/UserDaoImpl.java
+++ src/main/java/dushkof/seaWars/dao/impl/UserDaoImpl.java
@@ -1,26 +1,38 @@
 package dushkof.seaWars.dao.impl;
 
 import dushkof.seaWars.dao.UserDao;
+import org.assertj.core.util.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.util.StringUtils;
 
 public class UserDaoImpl implements UserDao {
-    private DriverManagerDataSource dataSource;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDaoImpl.class);
+    private JdbcTemplate jdbcTemplate;
 
 
     @Override
     public String createUser(String name, String password) {
         try {
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-            jdbcTemplate.execute("INSERT INTO users(NAME , PASSWORD) VALUES('" + name +"' ,'"+ password +"');");
+        Preconditions.checkArgument(!StringUtils.isEmpty(name), "Argument name cannot be empty");
+        Preconditions.checkArgument(!StringUtils.isEmpty(password), "Argument password cannot be empty");
+
+            getJdbcTemplate().execute("INSERT INTO users(NAME , PASSWORD) VALUES('" + name +"' ,'"+ password +"');");
+            LOGGER.info("User " + name + " is created");
             return "OK";
         } catch (Exception e) {
-            return e.getMessage();
+            LOGGER.error(e.getMessage(), e);
+            return "NOK";
 
         }
     }
 
-    public void setDataSource(DriverManagerDataSource dataSource) {
-        this.dataSource = dataSource;
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
