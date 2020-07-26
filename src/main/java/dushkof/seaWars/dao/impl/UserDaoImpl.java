40fbc17051faaf30903cda322bacbb4ecca60758
oiker <68709043+oiker@users.noreply.github.com>
2020-07-26 18:24:41 +0300

    Сохранение юзера
    Проверка пароля и логина

--- src/main/java/dushkof/seaWars/dao/impl/UserDaoImpl.java
+++ src/main/java/dushkof/seaWars/dao/impl/UserDaoImpl.java
@@ -1,8 +1,8 @@
 package dushkof.seaWars.dao.impl;
 
-import dushkof.seaWars.Objects.User;
+import dushkof.seaWars.objects.User;
 import dushkof.seaWars.dao.UserDao;
-import dushkof.seaWars.Objects.mappers.UserMapper;
+import dushkof.seaWars.objects.mappers.UserMapper;
 import org.assertj.core.util.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+
+import static java.lang.String.*;
 
 
 public class UserDaoImpl implements UserDao {
