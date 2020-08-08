e15430934f31bf7604a000beb0f272770eb3e609
oiker <68709043+oiker@users.noreply.github.com>
2020-08-08 15:35:12 +0300

    Отработка метода Shoot  определением победителя

--- src/main/java/dushkof/seaWars/services/impl/PlayServiceImpl.java
+++ src/main/java/dushkof/seaWars/services/impl/PlayServiceImpl.java
@@ -40,57 +40,51 @@ public class PlayServiceImpl implements PlayService {
 
         if ( cell.isChecked() == true ) {
             return "NOK";
-        } else {
+        }
+        cell.setChecked(true);
+        if ( cell.getStatus() == null ) {
             cell.setChecked(true);
-            if ( cell.getStatus() == null ) {
-                cell.setChecked(true);
-                cellRepo.save(cell);
-                return "MISS";
-            } else {
-                Long shipId = jdbcTemplate.queryForObject("SELECT ship_id FROM ship_all_cells WHERE all_cells_id =" + cell.getId() + ";", Long.class);
-                List<Cell> allCells = jdbcTemplate.queryForList("SELECT all_cells_id FROM ship_all_cells WHERE ship_id =" + shipId + ";", Cell.class);
-                Ship ship = shipRepo.getShipById(shipId);
-                ship.setAllCells(allCells);
-                List<Cell> woundedCells = ship.getWoundedCells();
-                int index = allCells.indexOf(cell);
-                woundedCells.add(allCells.get(index));
-                ship.setWoundedCells(woundedCells);
-                shipRepo.save(ship);
-                if ( allCells.size() == woundedCells.size() ) {
-                    ship.setAlive(false);
-                    shipRepo.save(ship);
-
-                    if (checkIfItWasTheLastShip(cellId)){
-                        return "Finish";
-                    }
-                    else{
-                    return "UBIL";}
-                } else {
-                    return "RANIL";
-                }
-            }
-
-        }}
-
-    public boolean checkIfItWasTheLastShip(Long cellId){
-        Cell cell = cellRepo.findCellById(cellId);
-        Long shipId = jdbcTemplate.queryForObject("SELECT ship_id FROM ship_all_cells WHERE all_cells_id =" + cell.getId() + ";",Long.class);
+            cellRepo.save(cell);
+            return "MISS";
+        }
+        Long shipId = jdbcTemplate.queryForObject("SELECT ship_id FROM ship_all_cells WHERE all_cells_id =" + cell.getId() + ";", Long.class);
         Ship ship = shipRepo.getShipById(shipId);
-        Long fieldId = jdbcTemplate.queryForObject("SELECT field_id FROM field_cells WHERE cells_id =" + cell.getId() + ";", Long.class);
-        Field field = fieldRepo.findFieldById(fieldId);
+        List<Cell> allCells = ship.getAllCells();
+        List<Cell> woundedCells = ship.getWoundedCells();
+        woundedCells.add(cell);
+        ship.setWoundedCells(woundedCells);
+        shipRepo.save(ship);
+        if ( allCells.size() == woundedCells.size() ) {
+            ship.setAlive(false);
+            shipRepo.save(ship);
+
+            if ( checkIfItWasTheLastShip(cellId) ) {
+                return "Finish";
+            } else {
+                return "UBIL";
+            }
+        } else {
+            return "RANIL";
+        }
+
+
+    }
+
+    public boolean checkIfItWasTheLastShip(Long cellId) {
+
+        Long fieldId = jdbcTemplate.queryForObject("SELECT field_id FROM field_cells WHERE cells_id =" + cellId + ";", Long.class);
+        Field field = fieldRepo.getOne(fieldId);
+        for (Ship ship : field.getShips()) {
+            if ( ship.isAlive() ) {
+                return false;
+            }
+        }
         Game game = gameRepo.findGameById(field.getGameID());
         User user = field.getUser();
-        List<Ship> shipsFromShip = jdbcTemplate.queryForList("SELECT ships_id FROM field_ships WHERE field_id =" + field.getId() + ";", Ship.class);
-        List<Cell> shipCells = ship.getAllCells();
-        fieldRepo.save(field);
-        userRepo.save(user);
-        if (shipsFromShip.size() == shipCells.size() ) {
-            game.setFinished(true);
-            game.setWinner(user.getName());
-            game.setFinishGame(new Date());
-            gameRepo.save(game);
-            return true;
-        }
-        else return false;
+        game.setFinished(true);
+        game.setWinner(user.getName());
+        game.setFinishGame(new Date());
+        gameRepo.save(game);
+        return true;
     }
 }
